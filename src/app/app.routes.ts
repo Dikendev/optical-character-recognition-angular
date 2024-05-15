b52162cc419d105681f37b0ae847a3f477f6755e
Dikendev <diken.dev@gmail.com>
2024-05-14 23:50:53 -0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ResponseComponent } from './pages/response/response.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+const data = {
+  title: 'Home',
+  showInNavBar: true,
+};
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    data: data,
+    component: HomeComponent,
+  },
+  {
+    path: 'response',
+    data: {
+      title: 'Response',
+      showInNavBar: true,
+    },
+    component: ResponseComponent,
+  },
+  {
+    path: '',
+    data: {
+      title: 'Home',
+      showInNavBar: false,
+    },
+    component: HomeComponent,
+  },
+];
