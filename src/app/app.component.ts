92101a34ff65adb1ba2804947e1b21bf26f1f0f1
Dikendev <diken.dev@gmail.com>
2024-05-14 23:51:09 -0300

    update app imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServerService } from './services/server.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    HttpClientModule,
+  ],
+  providers: [ServerService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'optical-character-recognition-angular';
